c5c32ef106c81b8901007fd9a9277a67d67408a8
Ignas-Cij <ignas.cijunaitis@wearemarketing.lt>
2023-07-19 17:03:11 +0300

    adapter changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,25 @@
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    // Use the Vercel adapter for deployment
-    adapter: adapter(),
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	},
 
-    // ... other kit configuration options
-
-  },
-
-  preprocess: [
-    vitePreprocess(),
-    preprocess({
-      scss: {
-        // options
-      },
-    }),
-  ],
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			scss: {
+				// options
+			},
+		}),
+	],
 };
 
 export default config;
